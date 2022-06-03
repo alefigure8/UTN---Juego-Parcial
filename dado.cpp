69d8aff9c67e543977af2c7a4941276ee47d8ead
ale_figure8 <gomeznieto@gmail.com>
2022-06-03 18:30:04 -0300

    terminando los dados

--- dado.cpp
+++ dado.cpp
@@ -1,53 +1,68 @@
 #include <iostream>
 using namespace std;
 
-void dados(int num);
+void dados(int *num, int cant_dados);
 
 int main (void){
 
-  dados(1);
-  dados(1);
-  dados(1);
+  int dado[3] = {5, 6, 2};
+
+  dados(dado, 3);
 
   return 0;
 }
 
-void dados(int num){
-  int dado[5][9];
+void dados(int *num, int cant_dados){
+
+  int dado_1 = num[0];
+  int dado_2 = num[1];
+  int dado_3 = num[2];
+
   for (int i = 0; i < 5; i++){
-    for (int j = 0; j < 9; j++){
+    for (int j = 0; j < ((cant_dados - 1)*10 + 9); j++){
 
       if(i == 0 || i == 4){
-        if(j == 0 || j == 8){
-          cout << "+";
-          if(j == 8){
-            cout << endl;
-          }
-        } else{
-          cout <<"-";
-        }
-      } else {
-        if(j == 0 || j == 8){
-          cout << "|";
-          if(j == 8){
-            cout << endl;
-          }
-        } else{
-         if(num % 2 != 0){
-          if((i == 2 && j == 4) || (((i == 1 && j == 2) || (i == 3 && j == 6)) && num > 1) || (((i == 1 && j == 6) || (i == 3 && j == 2)) && num > 3)){
-            cout << "x";
-            } else {
-              cout <<" ";
-            }
-         } else {
-           if(((i == 1 && j == 2) || (i == 3 && j == 6)) || (((i == 1 && j == 6) || (i == 3 && j == 2)) && num > 2) || (((i == 2 && j == 2) || (i == 2 && j == 6)) && num > 4)){
-            cout << "x";
+
+        int n1;
+        int n2;
+
+        for (int h = 0; h < cant_dados; h++){
+          n1 = 0 + (h*10);
+          n2 = 8 + (h*10);
+          if(j == n1 || j == n2){
+            cout << "+";
+            if(j == ((cant_dados - 1) * 10) + 8){
+                cout << endl;
+              }
+          } else if(j==((h * 10) + 9)){
+            cout << " ";
           } else {
-            cout <<" ";
+            if(h == cant_dados - 1 && j != 0 && j!=8 && j!=9 && j!= 10 && j!=18 && j!=19 && j!=20 && j!=28){
+              cout << "-";
+            }
           }
-         }
+        }
+
+      } else {
+        int n3;
+        int n4;
+        for (int h = 0; h < cant_dados; h++){
+          n3 = 0 + (h*10);
+          n4 = 8 + (h*10);
+          if(j == n3 || j == n4){
+            cout << "|";
+            if(j == ((cant_dados - 1) * 10) + 8){
+                cout << endl;
+              }
+          } else if(j==((h * 10) + 9)){
+            cout << " ";
+          } else {
+            if(h == cant_dados - 1 && j != 0 && j!=8 && j!=9 && j!= 10 && j!=18 && j!=19 && j!=20 && j!=28){
+              cout << " ";
+            }
+          }
         }
       }
-    }
+     }
   }
-}
+ }
--- menu.cpp
+++ menu.cpp
@@ -3,17 +3,28 @@
 #include <fstream>
 #include<locale>
 #include <iomanip>
+#include <string>
 using namespace std;
 
+/* Funciones */
 char menuInicial();
 void nombres(int x);
 void lines();
 void endLines(int x);
 void cerdo(int x, string dialog);
+void cerdo2(int x, string dialog);
 void ResizeConsole(int width, int height);
 void colorTexto (int x);
-string nombreJugador;
+void quienEmpieza();
+void dados(int *a, int b);
+void iniciarMatriz(int matriz[2][3], int filas, int columnas);
+void imprimirDados(int num);
 
+/* Variables globales */
+string jugadores[2];
+string jugadorActual;
+
+/* Main */
 int main (void){
   setlocale(LC_ALL, "spanish");
   ResizeConsole(850,550);
@@ -37,7 +48,7 @@ int main (void){
   return 0;
 }
 
-
+/* Funcion para el munnu de inicio */
 char menuInicial(){
 
   char eleccion;
@@ -77,6 +88,7 @@ char menuInicial(){
  void nombres(int x){
   string dialog = "";
 
+  // segun el turno cambia el dialogo
   if(x == 0){
     dialog = "\t\t¿Cuál es tu nombre, cerdo uno?";
   } else {
@@ -86,11 +98,11 @@ char menuInicial(){
   lines();
   colorTexto(15); cout << setw(10)<< "THE KING OF PIGS" ;
   endLines(6);
-  cerdo(x, dialog);
+  cerdo(x, dialog); // Imprime cerdo
   endLines(8);
   lines();
   endLines(2);
-  colorTexto(15); cin >> nombreJugador;
+  colorTexto(15); cin >> jugadores[x]; // Guarda nombre de los jugadores
  }
 
 
@@ -135,6 +147,28 @@ char menuInicial(){
                      `------')";
  }
 
+ /* Funcon que dibuja al cerdo*/
+ void cerdo2(int x, string dialog){
+  colorTexto(13); cout << R"(
+          _                           _
+          ;`.                       ,'/
+          |`.`-.      _____      ,-;,'|
+          |  `-.\__,-'     `-.__//'   |
+          |     `|               \ ,  |
+          `.  ```  _              ,  .'
+            \_`     `.     ,   ,  `_/)
+              \    0  `   ,   0 ` /
+               | '  |  ____  | , |
+               |     ,'    `.    |
+               |    (  O' O  )   |
+               `.    \__,.__/   ,'
+                 `-._  \__/   _,'
+                     `------')";
+
+  endLines(3);
+  colorTexto(12 + (x*(-3))); cout << setw(50)<< dialog;
+ }
+
 /* Funcion para darle ancho y largo a la consola*/
 void ResizeConsole(int width, int height){
 	HWND console = GetConsoleWindow();
@@ -147,4 +181,166 @@ void ResizeConsole(int width, int height){
 void colorTexto (int x){
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(h, x);
+}
+
+/* Funcion para determinar cual jugador comienza*/ 
+void quienEmpieza(){
+  int CANT_DADOS = 2;
+  int CANT_JUGADORES = 2;
+  int dados_jugadores[3];
+  int totalJugadores[2][3];
+  int sumaMaxima = 0;
+  int jugador;
+  int dadoMaximo = 0;
+  int jugadorDadoMaximo;
+  int totalJugador1= totalJugadores[0][0] + totalJugadores[0][1];
+  int totalJugador2= totalJugadores[1][0] + totalJugadores[1][1];
+  string dialog;
+
+  iniciarMatriz(totalJugadores, CANT_JUGADORES, CANT_DADOS);
+
+  char eleccion;
+
+  for (int i = 0; i < CANT_JUGADORES; i++){
+    /* Pantalla 1*/
+    lines();
+    endLines(8);
+    if(i == 0){
+      dialog = "Lanza los dados, " + jugadores[i] +  " ¿Estás listos? (S/N)";
+    } else {
+      dialog = "Ahora es tu turno " + jugadores[i] +  " ¿Estás listos? (S/N)";
+    }
+
+    cerdo2(i, dialog);
+
+    endLines(8);
+    lines();
+
+    cin >> eleccion;
+
+    /* Pantalla 2*/
+    lines();
+    endLines(8);
+  
+    if(eleccion == 'S'){
+
+      dados(dados_jugadores, 2);
+
+      for (int j = 0; j < CANT_DADOS; j++){
+        totalJugadores[i][j] += dados_jugadores[j];
+        imprimirDados(dados_jugadores[j]);
+      }
+    } else {
+      if(i == 0){ // Si no quiere tirar, cede turno
+       jugadorActual = jugadores[1];
+       i = 2;
+      } else {
+        jugadorActual = jugadores[0];
+        i = 2;
+      }
+    }
+
+    int total = 0;
+
+    for (int j = 0; j < CANT_DADOS; j++){
+      total += totalJugadores[i][j];
+
+      if(totalJugadores[i][j] > dadoMaximo){
+        dadoMaximo = totalJugadores[i][j];
+        jugadorDadoMaximo = i + 1;
+      }
+    }
+
+    string strTotal = to_string(total);
+    colorTexto(15);cout << setw(50) <<"Has sumado " + strTotal + "!";
+
+    endLines(8);
+    lines();
+
+    if(total > sumaMaxima){
+      sumaMaxima = total;
+      jugador = i + 1;
+    }
+
+    if(totalJugador1 != totalJugador2){
+        if (jugador == 1){
+        jugadorActual = jugadores[0];
+      } else {
+        jugadorActual = jugadores[1];
+      }
+    } else {
+      if (jugadorDadoMaximo == 1){
+        jugadorActual = jugadores[0];
+      } else {
+        jugadorActual = jugadores[1];
+      }
+    }
+  }
+
+  lines();
+  endLines(8);
+
+  dialog = "¡COMIENZA EL JUGADOR " + jugadorActual + "!";
+  cerdo2(0, dialog);
+
+  endLines(8);
+  lines();
+
+}
+
+/* Funcion para inicializar con ceros una matriz*/
+void iniciarMatriz(int matriz[2][3], int filas, int columnas){
+  for (int i = 0; i < filas; i++){
+    for (int j = 0; j < columnas; j++){
+      matriz[i][j] = 0;
+    }
+  }
+}
+
+/* Funcion que simula los dados y devuelve numeros random */
+void dados(int *a, int b){
+  srand(time(0));
+  for (int i = 0; i < b; i++){
+    a[i] = rand() % 6 + 1;
+  }
+}
+
+void imprimirDados(int num){
+  int dado[5][9];
+  for (int i = 0; i < 5; i++){
+    for (int j = 0; j < 9; j++){
+
+      if(i == 0 || i == 4){
+        if(j == 0 || j == 8){
+          cout << "+";
+          if(j == 8){
+            cout << endl;
+          }
+        } else{
+          cout <<"-";
+        }
+      } else {
+        if(j == 0 || j == 8){
+          cout << "|";
+          if(j == 8){
+            cout << endl;
+          }
+        } else{
+         if(num % 2 != 0){
+          if((i == 2 && j == 4) || (((i == 1 && j == 2) || (i == 3 && j == 6)) && num > 1) || (((i == 1 && j == 6) || (i == 3 && j == 2)) && num > 3)){
+            cout << "x";
+            } else {
+              cout <<" ";
+            }
+         } else {
+           if(((i == 1 && j == 2) || (i == 3 && j == 6)) || (((i == 1 && j == 6) || (i == 3 && j == 2)) && num > 2) || (((i == 2 && j == 2) || (i == 2 && j == 6)) && num > 4)){
+            cout << "x";
+          } else {
+            cout <<" ";
+          }
+         }
+        }
+      }
+    }
+  }
 }
